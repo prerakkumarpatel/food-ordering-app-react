6065f30cd30f7ab3ac4fa2c2331eaaa485273d35
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-06-26 00:06:42 -0400

    Orderable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isExpandList, setExpandList] = useState(true);
+  const [isOrderPlaced, setIsOrderPlaced] = useState(null);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
@@ -32,20 +35,70 @@ const Cart = (props) => {
       ))}
     </ul>
   );
+  const orderClickHandler = () => {
+    setIsCheckout(true);
+  };
+  const seeItemHandler = () => {
+    setExpandList(!isExpandList);
+  };
+  const submitHandler = (userData) => {
+    fetch("https://doordash-48d83-default-rtdb.firebaseio.com/orders.json", {
+      method: "POST",
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setIsCheckout(false);
+          setIsOrderPlaced(true);
+        }
+      })
+      .catch((error) => {
+        setIsOrderPlaced(false);
+      });
+  };
+  const modelAction = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderClickHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
 
   return (
     <Modal onClose={props.onCloseCart}>
-      {cartItems}
+      <span className={classes.viewList} onClick={seeItemHandler}>
+        {hasItems && isExpandList
+          ? "Close the Ordered Items "
+          : "See Ordered Items"}
+      </span>
+      {isExpandList && cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
+      <div>
+        {isCheckout && (
+          <Checkout onConfirm={submitHandler} onCancel={props.onCloseCart} />
+        )}
       </div>
+
+      {isOrderPlaced && (
+        <div>
+          <h1 className={classes.success}> You can watch Netflix and Chill.</h1>
+          <br /> We will deliver it to Your Door success <br /> Click anywhere
+          to close
+          <br />
+        </div>
+      )}
+      {!isCheckout && !isOrderPlaced && modelAction}
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,50 +1,69 @@
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-const mealsList = DUMMY_MEALS.map((meal) => (
-  <MealItem
-    id={meal.id} // this is new!
-    key={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-  />
-));
 
 export default function AvailableMeals(params) {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://doordash-48d83-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        console.log(key);
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+
+      setIsError(true);
+    });
+  }, []);
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        {<h1>Loading ....</h1>}
+      </section>
+    );
+  }
+  if (isError) {
+    return (
+      <section className={classes.mealsError}>
+        <h1>Error try again</h1>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id} // this is new!
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
     <>
       <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
+        <Card>{!isLoading && <ul>{mealsList}</ul>}</Card>
       </section>
     </>
   );
